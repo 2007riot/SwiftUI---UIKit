2bd1e2291f8145c693d7fe6dc8d11a97f4db9c89
Galina Aleksandrova <73304608+2007riot@users.noreply.github.com>
2022-06-15 16:32:50 +0200

    Update SheetPresentationForSwiftUI.swift

--- SheetPresentationForSwiftUI.swift
+++ SheetPresentationForSwiftUI.swift
@@ -107,6 +107,28 @@ extension View {
         }
 }
 
+struct halfSheetViewModifier<SwiftUIContent>: ViewModifier where SwiftUIContent: View {
+    
+    @Binding var isPresented: Bool
+    let onDismiss: (() -> Void)?
+    let swiftUIContent: SwiftUIContent
+    
+    init(isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, content: () -> SwiftUIContent) {
+        self._isPresented = isPresented
+        self.onDismiss = onDismiss
+        self.swiftUIContent = content()
+       
+    }
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            SheetPresentationForSwiftUI($isPresented,onDismiss: onDismiss) {
+                swiftUIContent
+            }.fixedSize()
+            content
+        }
+    }
+}
 
 
 
